5c7218ad7b26558e5c4e2f3309908a7af836b476
Numerica98 <00041517@uca.edu.sv>
2019-06-11 10:41:19 -0600

    Ventana inicial y de personajes creada y enlazada

---
+++ app/src/main/java/com/example/proyectopdm/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.proyectopdm
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bt_Next.setOnClickListener {
+            it.findNavController().navigate(R.id.toCharacter)
+        }
+    }
+
+
+}
